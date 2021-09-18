923dcfe56d2a9fd50bdd604f9262d3e966c35425
Hibanaga <kenobidobiwan@gmail.com>
2021-09-18 15:04:28 +0200

    add new branch

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const { MongoClient } = require("mongodb");
+
+app.use(cors());
+app.use(express.json());
+
+const username = `hibana_main`;
+const password = `Zaxscd1212`;
+const cluster = `clusterprojectlang.jt1vg`;
+const dbname = `myFirstDatabase`;
+
+const PORT = process.env.PORT || 3001;
+const URI = `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(URI);
+
+// async function run() {
+//   try {
+//     await client.connect();
+//   } catch (err) {
+//     console.log(err.stack);
+//   } finally {
+//     await client.close();
+//   }
+// }
+
+async function run() {
+  let dbName = "test";
+
+  try {
+    // let doc = {
+    //   name: "etuman",
+    // };
+    // await col.insertOne(doc);
+
+    await client.connect();
+    console.log("Connected correctly to server");
+
+    const db = client.db(dbName);
+    const col = db.collection("testConnect");
+
+    const myDoc = await col.findOne({ name: "Vladyslav" });
+
+    console.log(myDoc);
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/show_data", (req, res) => {
+  res.json({ message: "hello" });
+});
+
+app.listen(PORT, () => console.log(`server lister on port ${PORT}`));
